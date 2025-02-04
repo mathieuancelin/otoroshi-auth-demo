b1a1e67c30d3b4ae95ff0b3ececaa36fbbab8d17
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-02-04 22:15:40 +0100

    first import

--- index.js
+++ index.js
@@ -172,8 +172,9 @@ app.get('/', (req, res) => {
   const isBehindOto = req.get('otoroshi-proxied-host');
   const isBehindAuth = Object.keys(req.cookies || {}).filter(key => key.indexOf("oto-papps-global-oauth")).length > 0;
   const profile = req.get("Otoroshi-User-Profile");
+  const proto = req.get("Otoroshi-Protocol-In");
   const parsedProfile = !!profile ? jwt.decode(profile) : null;
-  console.log(isBehindOto, isBehindAuth, parsedProfile)
+  console.log(isBehindOto, isBehindAuth, req.cookies, proto, parsedProfile)
   res.status(200).contentType("text/html").send(indexHtml("Hey Stranger !", "This app is private, you shouldn't be able to view it !"));
 })
 
