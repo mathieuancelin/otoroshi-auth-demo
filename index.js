f129426df5c41df7f7649c6d62c87c934d2abc87
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-02-04 22:43:28 +0100

    first import

--- index.js
+++ index.js
@@ -159,11 +159,12 @@ function indexHtml(title, paragraph) {
         <p class="lead">
           <a href="https://maif.github.io/otoroshi/manual/entities/auth-modules.html" target="_blank" class="btn btn-lg btn-light fw-bold border-white bg-white">Learn more</a>
         </p>
-        <p>
-          <a href="https://app-59d810c1-e7cd-48fa-a6ab-a96f7d5e7191.cleverapps.io/">origin</a>
-          <a href="https://app-59d810c1-e7cd-48fa-a6ab-a96f7d5e7191.cleverapps.io/oto_only">origin oto_only</a>
-          <a href="https://demo-webauth.otoroshidemo.ovh/oto_only">oto_only</a>
-        </>
+        <div style="display: flex; flex-direction: column;">
+          <a class="text-white" href="https://app-59d810c1-e7cd-48fa-a6ab-a96f7d5e7191.cleverapps.io/">app direct access</a>
+          <a class="text-white" href="https://app-59d810c1-e7cd-48fa-a6ab-a96f7d5e7191.cleverapps.io/oto_only">app direct access /oto_only</a>
+          <a class="text-white" href="https://demo-webauth.otoroshidemo.ovh">app proxy access</a>
+          <a class="text-white" href="https://demo-webauth.otoroshidemo.ovh/oto_only">app proxy access /oto_only</a>
+        <div/>
       </main>
       <footer class="mt-auto text-white-50">
         <p>Auth. Demo for <a class="text-white" target="_blank" href="https://www.otoroshi.io">Otoroshi</a> by <a target="_blank" class="text-white" href="https://www.cloud-apim.com">Cloud APIM</a></p>
